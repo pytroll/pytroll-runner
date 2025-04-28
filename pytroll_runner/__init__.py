d17ebbec35257767660febd2b0a6c23fe359dc39
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-04-28 11:47:47 +0200

    Change the order of params in the signature of `generate_message()`

--- pytroll_runner/__init__.py
+++ pytroll_runner/__init__.py
@@ -89,14 +89,14 @@ def run_and_publish(config_file: Path, message_file: str | None = None):
             gen = run_from_message_file(command_to_call, message_file)
         for log_output, mda in gen:
             try:
-                message, preexisting_files = generate_message(publisher_config, log_output, mda, preexisting_files)
+                message, preexisting_files = generate_message(publisher_config, mda, log_output, preexisting_files)
                 logger.debug(f"Sending message = {message}")
                 pub.send(str(message))
             except FileNotFoundError:
                 logger.debug("We could find not any new files, so no message will be sent.")
 
 
-def generate_message(publisher_config, log_output, mda, preexisting_files):
+def generate_message(publisher_config, mda, log_output, preexisting_files):
     """Generate message from either the log output or existing files."""
     try:
         message = generate_message_from_log_output(publisher_config, mda, log_output)
